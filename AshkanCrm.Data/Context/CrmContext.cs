bbf670cfe798a32c3be333b4d767939efc7857b8
ashkan20171 <ashkan.mataee@yahoo.com>
2026-02-13 11:27:54 +0330

    add project

---
+++ AshkanCrm.Data/Context/CrmContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyCrm.Web
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext(DbContextOptions<CrmContext> options)
+            : base(options)
+        {
+        }
+
+        // نمونه:
+        // public DbSet<Customer> Customers { get; set; }
+    }
+}
